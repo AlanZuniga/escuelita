767a9aefa426e75b5ac292c301989915e365e714
Alan Zuñiga <alan.zuniga@alumnos.udg.mx>
2025-03-15 21:04:19 -0600

    agregue show-alumno.blade.php

---
+++ resources/views/alumnos/show-alumno.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Descripción Alumno</title>
+</head>
+<body>
+    <h1>Alumno # {{ $alumno->id }}</h1>
+
+    <ul>
+        <li>Nombre: {{ $alumno->nombre }}</li>
+        <li>Correo: {{ $alumno->correo }}</li>
+        <li>Fecha_Nacimiento: {{ $alumno->Fecha_Nacimiento }}</li>
+        <li>Ciudad: {{ $alumno->Ciudad }}</li>
+
+    </ul>
+    <p>
+        Alumno: <br>
+        {{ $alumno->alumno }}
+    </p>
+</body>
+</html>
